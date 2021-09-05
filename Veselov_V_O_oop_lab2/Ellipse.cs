cad9140e86a96e78af4df27a007e920e673492b7
Vasyl Veselov <vasyl_veselov@epam.com>
2021-09-05 12:49:54 +0300

    Add project files.

---
+++ Veselov_V_O_oop_lab2/Ellipse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veselov_V_O_oop_lab2
+{
+    class Ellipse
+    {
+        private double radiusB;
+        private double radiusA;
+        private double square;
+
+        public void setRadiusB(double rB) {
+            this.radiusB = rB;
+        }
+        public void setRadiusA(double rA) {
+            this.radiusA = rA;
+        }
+        public double getRadiusB() {
+            return radiusB;
+        }
+        public double getRadiusA() {
+            return radiusA;
+        }
+        public double getSquare() {
+            square = radiusB * radiusA * Math.PI;
+            return square;
+        }
+
+        public Ellipse(double rA, double rB) {
+            radiusA = rA;
+            radiusB = rB;
+        }
+        public Ellipse() { }
+    }
+}
---
+++ Veselov_V_O_oop_lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Veselov_V_O_oop_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ellipse ellipse = new Ellipse(3, 5);
+            Ellipse ellipse = new Ellipse();
+            ellipse.setRadiusA(3);
+            ellipse.setRadiusB(5);
+            double square = ellipse.getSquare();
+            Console.WriteLine("Ellipse square is " + square);
+
+            //instead of system.pause()
+            Console.ReadLine();
+        }
+    }
+}
